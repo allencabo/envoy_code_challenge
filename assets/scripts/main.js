23c1ab680aedd55aa0ceb274669414ce5679b374
Allen <encabo.allen@gmail.com>
2017-06-30 13:52:10 -0700

    Set up grid system. style the rest of the page out

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,6 @@
-/** import external dependencies */
-
 $(document).ready(function() {
     $('.menu__toggle').click(function() {
         $(this).toggleClass('active')
         $('.menu__overlay').toggleClass('open');
     });
 });
-
-/** Load Events */
-jQuery(document).ready(() => routes.loadEvents());
