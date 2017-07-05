f7b55161941c10e04589b24e165d5b9c1e198f88
Allen <encabo.allen@gmail.com>
2017-07-05 11:34:37 -0700

    Add Slick Carousel. Style according to mock.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+    // Toggle Menu
     $('.menu__toggle').click(function() {
         $(this).toggleClass('active')
         $('.menu__overlay').toggleClass('open');
+    });
+    //Slick Slider
+    $('.hero__slider').slick({
+        draggable: true,
+        arrows: true,
+        dots: true
     });
 });
